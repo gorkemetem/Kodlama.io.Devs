705166b728c0b476fff75f928eeac30e4ca4b510
Görkem Etem Irmak <gorkemetem98@gmail.com>
2022-09-11 12:35:46 +0300

    Create layers and add migration

---
+++ Application/Service/Repositories/ILanguageRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+
+namespace Application.Service.Repositories
+{
+    public interface ILanguageRepository : IAsyncRepository<Language>, IRepository<Language>
+    {
+    }
+}
---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Language> Languages { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Language>(a =>
+            {
+                a.ToTable("Languages").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+
+            Language[] languageEntitySeed = { new(1, "C#"), new(2,("Java"))};
+            modelBuilder.Entity<Language>().HasData(languageEntitySeed);
+
+        }
+    }
+}
